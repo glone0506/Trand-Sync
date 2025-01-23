5822d418385b5f36629ca8a580e7327edbc953a7
xixeonxim <xixeonxim@duksung.ac.kr>
2025-01-23 15:07:15 +0900

    ADD: TAB Component

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import Example from "@/components/carousel";
-import "bootstrap/dist/css/bootstrap.min.css";
-export default function Page() {
-  return (
-    <div>
-      <Example />
-    </div>
-  );
-}
