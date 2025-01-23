d07d48f0323bd3c2c36d6a435573627bf1c4c8d0
xixeonxim <xixeonxim@duksung.ac.kr>
2025-01-23 16:25:23 +0900

    chore : 파일 추가

--- app/page.js
+++ app/page.js
@@ -1,14 +1,6 @@
-"use client";
-
-import ControlledCarousel from "@/components/mainpage/Carousel";
+import ControlledCarousel from "@/components/carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
-import TabBar from "@/components/mainpage/Tabs";
-
-const posts = [
-  { id: 1, title: "첫 번째 게시물", content: "첫 게시물입니다." },
-  { id: 2, title: "두 번째 게시물", content: "두 번째 게시물입니다." },
-  { id: 3, title: " 세 번째 게시물", content: "세 번째 게시물입니다." },
-];
+import TabBar from "@/components/tabs";
 
 export default function Page() {
   return (
