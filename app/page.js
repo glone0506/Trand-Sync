1c52fe505d91e2903fd1d5e6e6d636947a44d486
glone <glone0506@gmail.com>
2025-01-23 14:35:57 +0900

    BootStrap Setting

--- app/page.js
+++ app/page.js
@@ -1,34 +1,9 @@
-import Carousels from "@/components/carousel";
-
-const carouselData = [
-  {
-    ITEM_CD_DL: "1",
-    EXP_CVaRNTS: "Exp 1",
-    HR_ITEM_NM: "Item 1",
-    LOC: "Location 1",
-    ITEM_KR_NM: "아이템 1",
-  },
-  {
-    ITEM_CD_DL: "2",
-    EXP_CVaRNTS: "Exp 2",
-    HR_ITEM_NM: "Item 2",
-    LOC: "Location 2",
-    ITEM_KR_NM: "아이템 2",
-  },
-  {
-    ITEM_CD_DL: "3",
-    EXP_CVaRNTS: "Exp 3",
-    HR_ITEM_NM: "Item 3",
-    LOC: "Location 3",
-    ITEM_KR_NM: "아이템 3",
-  },
-  // 추가 데이터...
-];
-
+import Example from "@/components/carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
 export default function Page() {
   return (
     <div>
-      <Carousels carouselData={carouselData} />{" "}
+      <Example />
     </div>
   );
 }
