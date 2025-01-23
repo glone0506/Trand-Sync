73378f4087e1ff7f7d03038972abcb218fa1c4e2
bonun00 <lifesciences00@naver.com>
2025-01-23 15:17:24 +0900

    임시

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
 import ControlledCarousel from "@/components/carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TabBar from "@/components/tabs";
+import PostList from "@/app/ui/mainpage/postList";
+
+export default function Page() {
 
 const posts = [
   { id: 1, title: '첫 번째 게시물', content: '첫 게시물입니다.' },
