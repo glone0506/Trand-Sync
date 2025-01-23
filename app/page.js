0ab423dd29c4db8ea0292295a53b0cc226d2bd35
[devbae1101] <[dev.bae1101@gmail.com]>
2025-01-23 11:26:08 +0900

    1.Default Layout 수정

--- app/page.js
+++ app/page.js
@@ -7,7 +7,6 @@ export default function Home() {
         <Image
           className="dark:invert"
           src="/next.svg"
-          alt="Next.js logo"
           width={180}
           height={38}
           priority
